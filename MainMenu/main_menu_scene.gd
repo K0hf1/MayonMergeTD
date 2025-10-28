b8ce73b135f0fe9dc62c3df365d98a75da23eb5a
Ninoabela <150281823+Ninoabela@users.noreply.github.com>
2025-10-29 02:44:01 +0800

    main menu

---
+++ MainMenu/main_menu_scene.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
